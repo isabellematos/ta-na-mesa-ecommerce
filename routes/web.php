0f9367b901ffb615aac540665d40c474d44501d3
Lais <laislivia0441@gmail.com>
2025-11-08 12:57:30 -0300

    feat: Implementa a funcionalidade básica de Login com Controller e View

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function(){
@@ -9,3 +10,7 @@ Route::get('/', function(){
 
 Route::get('/create-user',[UserController::class,'create'])->name('user.create');
 Route::post('/store-user',[UserController::class,'store'])->name('user.store');
+
+Route::prefix('auth')->group(function(){
+    Route::get('/login', [\App\Http\Controllers\auth\LoginController::class, 'login'])->name('login');
+});
