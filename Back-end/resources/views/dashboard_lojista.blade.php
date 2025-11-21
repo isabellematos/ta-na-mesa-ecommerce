6244f3fe23f6e02207ea929fee647b2548885bc9
Luana Miyashiro <152427133+miyamiyashiro@users.noreply.github.com>
2025-11-20 23:49:22 -0300

    dashboard_lojista.blade.php
    criei página do lojista no view. tem q fazer a rota

---
+++ Back-end/resources/views/dashboard_lojista.blade.php
@@ -0,0 +1,244 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Seu perfil - Lojista</title>
+
+    <link rel="stylesheet" href="{{ asset('assets/css/estiloMain.css') }}">
+    <link rel="stylesheet" href="{{ asset('components/atoms/button.css') }}">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('components/atoms/input.css') }}">
+    <link rel="stylesheet" href="{{ asset('components/atoms/label.css') }}">
+    <link rel="stylesheet" href="{{ asset('components/molecules/form-group.css') }}">
+    <link rel="stylesheet" href="{{ asset('components/molecules/product-card.css') }}">
+    <link rel="stylesheet" href="{{ asset('components/organisms/footer.css') }}">
+    <link rel="stylesheet" href="{{ asset('components/organisms/header.css') }}">
+    <link rel="stylesheet" href="{{ asset('components/organisms/product-grid.css') }}">
+</head>
+
+<body>
+
+    <header class="header">
+        <nav class="navbar">
+            <div class="navbar-content">
+                <div class="logo">
+                    <img src="../assets/img/logoDaora.png" alt="Logo Ta Na Mesa">
+                </div>
+                <div class="user-actions">
+                    <a href="#"><img src="{{ asset('assets/img/gatoMago.jpg') }}" alt="Perfil do usuário"></a>
+                    <a href="#" class="perfilLojista" alt="Finalizar sessão">Sair</a>
+                </div>
+            </div>
+        </nav>
+    </header>
+
+    <main class="main-content">
+        <section class="hero-banner">
+            <div class="banner-image">
+                <img src="{{ asset('assets/img/Banner perfil.png') }}" alt="Banner com personagens de RPG">
+                <h1 class="banner-title-seller">Seu perfil - Lojista</h1>
+            </div>
+
+            <div class="scroll-down-container">
+                <a href="#store-section" class="scroll-down-button">
+                    <img src="{{ asset('assets/img/botao.png') }}" alt="Scroll para baixo">
+                </a>
+            </div>
+
+
+            <section id="store-section" class="store">
+
+                <section id="profile-seller-infos" class="seller-infos">
+                    <h2>Suas informações</h2>
+
+                    <div class="seller-info-content">
+
+                        <div class="seller-photo">
+                            <img src="../assets/img/gatoMago.jpg" alt="Foto do vendedor">
+                        </div>
+
+                        <div class="seller-fields">
+
+                            <div class="field-row">
+                                <label>Foto de perfil:</label>
+                                <div class="input-upload-container">
+                                    <input type="file" id="profile-upload" style="display: none;">
+                                    <button type="button" class="btn-upload"
+                                        onclick="document.getElementById('profile-upload').click()">FAÇA O
+                                        UPLOAD</button>
+                                </div>
+                            </div>
+
+                            <div class="field-row">
+                                <label>Link para contato:</label>
+                                <input type="url">
+                            </div>
+
+                            <div class="field-row">
+                                <label>Breve descrição:</label>
+                                <div class="textarea-wrapper">
+                                    <textarea rows="3"></textarea>
+                                </div>
+                            </div>
+
+                            <div class="seller-save-btn">
+                                <button class="btn-primary">Salvar</button>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+
+                <section id="seller-salesAds" class="seller-ads">
+
+
+                    <h2 class="sellers-title">Seus anuncios</h2>
+
+                    <div class="filter-bar">
+                        <div class="filter-header">
+                            <p class="filter-title">Filtro</p>
+                            <div class="filter-actions">
+                                <button class="btn-reset">Resetar</button>
+                                <button class="btn-apply">Aplicar</button>
+                            </div>
+                        </div>
+                        <div class="filter-line"></div>
+                        <div class="filter-row">
+                            <div class="filter-group">
+                                <label for="data">Data:</label>
+                                <div class="input-container">
+                                    <input type="date" id="data">
+                                    <button class="btn-reset-group">Resetar</button>
+                                </div>
+                            </div>
+                            <div class="filter-group">
+                                <label for="sistema">Sistema:</label>
+                                <div class="input-container">
+                                    <select id="sistema"></select>
+                                    <button class="btn-reset-group">Resetar</button>
+                                </div>
+                            </div>
+                            <div class="filter-group">
+                                <label for="localizacao">Localização:</label>
+                                <div class="input-container">
+                                    <select id="localizacao"></select>
+                                    <button class="btn-reset-group">Resetar</button>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="filter-category">
+                            <label>Categoria:</label>
+                            <div class="category-buttons">
+                                <button class="btn-category active">Fantasia</button>
+                                <button class="btn-category">Terror</button>
+                                <button class="btn-category">Medieval</button>
+                                <button class="btn-category">Aventura</button>
+                                <button class="btn-category">Investigação</button>
+                            </div>
+                        </div>
+                    </div>
+                    <button class="btn-ads" id="openModalButton">Adicionar novo anuncio</button>
+                </section>
+            </section>
+
+
+
+            <!-- MODAL -->
+
+            <div id="productModal" class="modal-overlay">
+                <div class="modal-content">
+                    <button class="modal-close-btn">&times;</button>
+
+                    <h2>Insira informações sobre o produto</h2>
+
+                    <div class="modal-form-grid">
+
+                        <label for="modal-nome">Nome:</label>
+                        <input type="text" id="modal-nome" placeholder="Nome do produto" class="modal-input-textarea">
+
+                        <label for="modal-preco">Preço:</label>
+                        <input type="text" id="modal-preco" placeholder="R$ 0,00" class="modal-input-textarea">
+
+                        <label for="modal-qtd">Quantidade:</label>
+                        <input type="number" id="modal-qtd" class="modal-input">
+
+                        <label for="modal-tags">Tags:</label>
+                        <input type="text" id="modal-tags" placeholder="Ex: Miniaturas, DnD, MESA"
+                            class="modal-input-textarea">
+
+                        <label>Foto do produto:</label>
+                        <button class="modal-btn-upload">Escolher Imagem</button>
+
+                        <label for="modal-variacao">Variação:</label>
+                        <select id="modal-variacao" class="modal-input">
+                            <option>Escolher variação</option>
+                        </select>
+
+                        <label for="modal-categoria">Categoria:</label>
+                        <select id="modal-categoria" class="modal-input">
+                            <option>Escolher categoria</option>
+                        </select>
+
+                        <label for="modal-descricao">Descrição:</label>
+                        <textarea id="modal-descricao" placeholder="Informações sobre o produto"
+                            class="modal-input-textarea"></textarea>
+
+                    </div>
+
+                    <button class="modal-btn-primary">Criar Anúncio!</button>
+                </div>
+            </div>
+
+            <script>
+                document.addEventListener('DOMContentLoaded', function () {
+                    const modal = document.getElementById('productModal');
+                    const openModalBtn = document.getElementById('openModalButton');
+
+                    if (!modal) {
+                        console.error("Erro: Elemento modal com ID 'productModal' não encontrado.");
+                        return;
+                    }
+
+                    const closeBtn = modal.querySelector('.modal-close-btn');
+
+                    function openModal() {
+                        modal.style.display = 'flex';
+                    }
+
+                    function closeModal() {
+                        modal.style.display = 'none';
+                    }
+
+                    if (openModalBtn) {
+                        openModalBtn.addEventListener('click', openModal);
+                    } else {
+                        console.warn("Erro, verificar botao.");
+                    }
+
+                    if (closeBtn) {
+                        closeBtn.addEventListener('click', closeModal);
+                    }
+
+                    modal.addEventListener('click', (event) => {
+                        if (event.target === modal) {
+                            closeModal();
+                        }
+                    });
+
+                    document.addEventListener('keydown', (event) => {
+                        if (event.key === 'Escape') {
+                            closeModal();
+                        }
+                    });
+
+                    closeModal();
+
+                });
+            </script>
+
+</body>
+
+</html>
